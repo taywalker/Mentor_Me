c6f8b77eb6eb628d333f9db9a84b2a99e0d7c4b0
taywalker <106616846+taywalker@users.noreply.github.com>
2024-11-11 17:44:00 -0600

    Sign in Page so far

--- mentor_me/src/pages/SignIn.js
+++ mentor_me/src/pages/SignIn.js
@@ -1,13 +1,26 @@
 // src/pages/SignIn.js
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/SignIn.css'; // Adjusted path to the styles folder
+import TextInput from '../components/TextInput';
 
 const SignIn = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   return (
     <div className="container">
       <div className="box">
-        <h2>Sign In</h2>
-        {/* Add input fields, buttons, or other content here */}
+        <h3>Sign In</h3>
+        <p>Don't have an account yet? <b>Sign up here!</b></p>
+        <TextInput 
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          placeholder="Your username or email"
+        /> 
+        <TextInput 
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+        />
       </div>
     </div>
   );
