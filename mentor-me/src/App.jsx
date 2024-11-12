50668919223d2b0fbefd883affcdd56ef370c562
Prosper Banda <prosperbanda@Prospers-MacBook-Pro.local>
2024-11-11 23:08:28 -0600

    Create the UI for the login and signup pages

---
+++ mentor-me/src/App.jsx
@@ -0,0 +1,37 @@
+import "./App.css";
+import Login from "./components/loginpage";
+import Signup from "./components/signup";
+import { useState, useEffect } from "react";
+import { UserContext } from "./UserContext";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function App() {
+  const [user, setUser] = useState(() => {
+    const storedUser = localStorage.getItem("user");
+    return storedUser ? JSON.parse(storedUser) : null;
+  });
+
+  const updateUser = (newUser) => {
+    setUser(newUser);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(user));
+  }, [user]);
+
+  return (
+    <div className="App">
+      <UserContext.Provider value={{ user, updateUser }}>
+        <BrowserRouter>
+          <Routes>
+            {/* <Route path="/home" element={user ? <Home/> : <Login />}/> */}
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/" element={<Login />} />
+          </Routes>
+        </BrowserRouter>
+      </UserContext.Provider>
+    </div>
+  );
+}
+
+export default App;
---
+++ mentor-me/src/components/signup.jsx
@@ -0,0 +1,109 @@
+import React, { useState, useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "./signup.css";
+import { UserContext } from "../UserContext.js";
+
+function Signup() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { updateUser } = useContext(UserContext);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await fetch(`http://localhost:3000/users`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, email, password }),
+        credentials: "include",
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        const loggedInUser = data.user;
+        localStorage.setItem("id", loggedInUser.id);
+        setUsername("");
+        setEmail("");
+        setPassword("");
+        updateUser(loggedInUser);
+        navigate("/");
+      } else {
+        alert("Signup failed");
+      }
+    } catch (error) {
+      alert("Signup failed: " + error);
+    }
+  };
+
+  return (
+    <div className="wrapper">
+      <div className="content">
+        <h1>Sign Up ✍🏼</h1>
+        <p className="welcome">Enter your information below</p>
+        <div className="user-information">
+          <form action="" onSubmit={handleSubmit}>
+            <div className="user-name">
+              <label htmlFor="name">
+                <strong>Full Name</strong>
+              </label>
+              <input
+                type="text"
+                placeholder="Enter Your Name"
+                value={username}
+                name="name"
+                onChange={(e) => setUsername(e.target.value)}
+                required
+              />
+            </div>
+            <div className="user-email">
+              <label htmlFor="email">
+                <strong>Email</strong>
+              </label>
+              <input
+                type="email"
+                placeholder="Enter Email"
+                value={email}
+                name="email"
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+            <div className="user-password">
+              <label htmlFor="password">
+                <strong>Password</strong>
+              </label>
+              <input
+                type="password"
+                placeholder="Enter Password"
+                value={password}
+                name="password"
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+            <button type="submit" className="btn-success">
+              Sign up
+            </button>
+            <p>You agree to our terms and policies</p>
+            <Link to="/" className="login-btn">
+              Login
+            </Link>
+          </form>
+        </div>
+      </div>
+      <div className="image-container">
+        <img
+          src="https://www.genesecloud.academy/wp-content/uploads/2021/02/mentor-1.svg"
+          alt="Mentorship"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Signup;
